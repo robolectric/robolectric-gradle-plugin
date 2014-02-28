e24a20f73d1a4c21bbeee31176844188bc82efbc
Mike Grafton <pair+mike@pivotallabs.com>
2014-02-28 14:13:15 -0800

    Added a slightly more interesting unit test

--- src/test/groovy/com/squareup/gradle/android/AndroidTestPluginTest.groovy
+++ src/test/groovy/com/squareup/gradle/android/AndroidTestPluginTest.groovy
@@ -3,27 +3,43 @@ package com.squareup.gradle.android
 import org.fest.assertions.api.Assertions
 import org.gradle.api.Project
 import org.gradle.testfixtures.ProjectBuilder
+import org.junit.Assert
 import org.junit.Test
 
 class AndroidTestPluginTest {
+
   @Test public void pluginDetectsAppPlugin() {
-    Project project = ProjectBuilder.builder().build();
+    Project project = ProjectBuilder.builder().build()
     project.apply plugin: 'android'
     project.apply plugin: 'android-test'
   }
 
   @Test public void pluginDetectsLibraryPlugin() {
-    Project project = ProjectBuilder.builder().build();
+    Project project = ProjectBuilder.builder().build()
     project.apply plugin: 'android-library'
     project.apply plugin: 'android-test'
   }
 
   @Test public void pluginFailsWithoutAndroidPlugin() {
-    Project project = ProjectBuilder.builder().build();
+    Project project = ProjectBuilder.builder().build()
     try {
       project.apply plugin: 'android-test'
     } catch (IllegalStateException e) {
       Assertions.assertThat(e).hasMessage("The 'android' or 'android-library' plugin is required.");
     }
   }
+
+  @Test public void applyingPluginCreatesATestTaskForTheDebugVariant() {
+    Project project = ProjectBuilder.builder().withProjectDir(new File("src/test/fixtures/android_app")).build();
+    project.apply plugin: 'android'
+    project.apply plugin: 'android-test'
+    project.android {
+      compileSdkVersion 19
+      buildToolsVersion "19.0.1"
+    }
+    project.evaluate()
+    def testDebugTask = project.tasks.testDebug
+    Assert.assertTrue(testDebugTask instanceof org.gradle.api.tasks.testing.Test)
+  }
+
 }
