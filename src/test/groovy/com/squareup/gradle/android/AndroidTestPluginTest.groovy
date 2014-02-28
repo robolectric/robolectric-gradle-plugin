59b1c3519d08becbf5dd8ddc599275df88ea5dce
Mike Grafton <pair+mike@pivotallabs.com>
2014-02-28 14:29:35 -0800

    Added test for the setting of the excludes pattern

--- src/test/groovy/com/squareup/gradle/android/AndroidTestPluginTest.groovy
+++ src/test/groovy/com/squareup/gradle/android/AndroidTestPluginTest.groovy
@@ -3,10 +3,22 @@ package com.squareup.gradle.android
 import org.fest.assertions.api.Assertions
 import org.gradle.api.Project
 import org.gradle.testfixtures.ProjectBuilder
-import org.junit.Assert
 import org.junit.Test
 
+import static org.junit.Assert.assertTrue
+
 class AndroidTestPluginTest {
+
+  public Project evaluatableProject() throws Exception {
+    Project project = ProjectBuilder.builder().withProjectDir(new File("src/test/fixtures/android_app")).build();
+    project.apply plugin: 'android'
+    project.apply plugin: 'android-test'
+    project.android {
+      compileSdkVersion 19
+      buildToolsVersion "19.0.1"
+    }
+    return project
+  }
 
   @Test public void pluginDetectsLibraryPlugin() {
     Project project = ProjectBuilder.builder().build()
@@ -23,17 +35,22 @@ class AndroidTestPluginTest {
     }
   }
 
-  @Test public void applyingPluginCreatesATestTaskForTheDebugVariant() {
-    Project project = ProjectBuilder.builder().withProjectDir(new File("src/test/fixtures/android_app")).build();
-    project.apply plugin: 'android'
-    project.apply plugin: 'android-test'
-    project.android {
-      compileSdkVersion 19
-      buildToolsVersion "19.0.1"
-    }
+  @Test public void createsATestTaskForTheDebugVariant() {
+    project = evaluatableProject()
     project.evaluate()
     def testDebugTask = project.tasks.testDebug
-    Assert.assertTrue(testDebugTask instanceof org.gradle.api.tasks.testing.Test)
+    assertTrue(testDebugTask instanceof org.gradle.api.tasks.testing.Test)
   }
 
+  @Test public void supportsSettingAnExcludePattern_viaTheAndroidTestExtension() {
+    project = evaluatableProject()
+
+    project.androidTest {
+      exclude "**/lame_tests/**"
+    }
+
+    project.evaluate()
+    def testDebugTask = project.tasks.testDebug
+    assertTrue(testDebugTask.getExcludes().contains("**/lame_tests/**"))
+  }
 }
