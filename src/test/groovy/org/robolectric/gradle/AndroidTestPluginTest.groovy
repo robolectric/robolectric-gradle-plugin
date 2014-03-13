a96d0e23fa15ce1463431cd80f71c12585afb993
Terry Chen <terry@pivotallabs.com>
2014-03-13 11:29:31 -0700

    Add unit tests for IDE support of different flavors

--- src/test/groovy/org/robolectric/gradle/AndroidTestPluginTest.groovy
+++ src/test/groovy/org/robolectric/gradle/AndroidTestPluginTest.groovy
@@ -1,10 +1,10 @@
 package org.robolectric.gradle
 
-import org.fest.assertions.api.Assertions
 import org.gradle.api.Project
 import org.gradle.testfixtures.ProjectBuilder
 import org.junit.Test
 
+import static org.fest.assertions.api.Assertions.assertThat
 import static org.junit.Assert.assertNotNull
 import static org.junit.Assert.assertTrue
 
@@ -23,7 +23,7 @@ class AndroidTestPluginTest {
         try {
             project.apply plugin: 'android-test'
         } catch (IllegalStateException e) {
-            Assertions.assertThat(e).hasMessage("The 'android' or 'android-library' plugin is required.");
+            assertThat(e).hasMessage("The 'android' or 'android-library' plugin is required.");
         }
     }
 
@@ -51,10 +51,29 @@ class AndroidTestPluginTest {
     @Test public void createsGenericTestClassesTask() {
         Project project = evaluatableProject()
         project.evaluate()
-
         assertNotNull(project.tasks.testClasses)
     }
 
+    @Test public void dumpsAllTestClassFilesAndResourcesIntoTheSameDirectory() {
+        Project project = evaluatableProject()
+        project.android {
+            productFlavors {
+                prod {
+                }
+                beta {
+                }
+            }
+        }
+        project.evaluate()
+        def expectedDestination = project.files("$project.buildDir/test-classes").singleFile
+
+        assertThat(project.tasks.compileTestProdDebugJava.destinationDir).isEqualTo(expectedDestination)
+        assertThat(project.tasks.compileTestBetaDebugJava.destinationDir).isEqualTo(expectedDestination)
+        assertThat(project.tasks.testProdDebugClasses.destinationDir).isEqualTo(expectedDestination)
+        assertThat(project.tasks.testBetaDebugClasses.destinationDir).isEqualTo(expectedDestination)
+        assertThat(project.tasks.processTestProdDebugResources.destinationDir).isEqualTo(expectedDestination)
+        assertThat(project.tasks.processTestBetaDebugResources.destinationDir).isEqualTo(expectedDestination)
+    }
 
     @Test public void uniqueTaskCreatedForEachFlavor() {
         Project project = evaluatableProject()
