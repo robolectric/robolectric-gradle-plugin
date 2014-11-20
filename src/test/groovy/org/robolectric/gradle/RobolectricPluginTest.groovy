8aa3c42241d1005ba1fae3e1a80801b79e2ea51e
Erich Douglass <erich.douglass@gmail.com>
2014-11-20 11:28:06 -0800

    Switch from fest-assert to assertj.

--- src/test/groovy/org/robolectric/gradle/RobolectricPluginTest.groovy
+++ src/test/groovy/org/robolectric/gradle/RobolectricPluginTest.groovy
@@ -7,7 +7,7 @@ import org.junit.Rule
 import org.junit.Test
 import org.junit.rules.ExpectedException
 
-import static org.fest.assertions.api.Assertions.assertThat
+import static org.assertj.core.api.Assertions.assertThat
 
 class RobolectricPluginTest {
     @Rule
