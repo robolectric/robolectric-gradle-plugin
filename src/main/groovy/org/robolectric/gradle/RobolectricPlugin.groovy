95ff18d3d75f729d9c4f387a6d231713ccf9f030
Christian Becker <christian.becker@qnips.com>
2015-05-18 18:49:18 +0200

    Added casts to some variables
    Previously its types couldn't be inferred. With the strong casts the IDE can now support auto completion.

--- src/main/groovy/org/robolectric/gradle/RobolectricPlugin.groovy
+++ src/main/groovy/org/robolectric/gradle/RobolectricPlugin.groovy
@@ -1,7 +1,9 @@
 package org.robolectric.gradle
 
+import com.android.build.gradle.api.BaseVariant
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.tasks.testing.Test
 
 /**
  * Robolectric gradle plugin.
@@ -14,7 +16,7 @@ class RobolectricPlugin implements Plugin<Project> {
         def configuration = new Configuration(project)
         project.afterEvaluate {
             // Configure the test tasks
-            configuration.variants.all { variant ->
+            configuration.variants.all { BaseVariant variant ->
                 def taskName = "test${variant.name.capitalize()}"
                 def assets = variant.mergeAssets.outputDir
                 def manifest = variant.outputs.first().processManifest.manifestOutputFile
@@ -22,7 +24,7 @@ class RobolectricPlugin implements Plugin<Project> {
                 def packageName = project.android.defaultConfig.applicationId
 
                 // Set RobolectricTestRunner properties
-                def task = project.tasks.findByName(taskName)
+                def task = project.tasks.findByName(taskName) as Test
                 task.systemProperty "android.assets", assets
                 task.systemProperty "android.manifest", manifest
                 task.systemProperty "android.resources", resources
